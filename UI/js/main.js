3843934e40474f2a5ab0e64f040b0ffa07c123fd
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-10 17:40:50 +0300

    [#161049094] Add javascript to toggle open and close the left side nav-bar

---
+++ UI/js/main.js
@@ -0,0 +1,7 @@
+// js function to open the side navbar
+
+function openSideBar(){
+    document.getElementById("left-nav").style.width="80px";
+    document.getElementById("main").style.marginLeft="80px";
+
+}
