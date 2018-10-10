d40ff27694ba387d9a207863cb5e0f5d7242f4ab
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-10 18:45:51 +0300

    [#161049094] Add javascript function to allow searching the inventory

--- UI/js/main.js
+++ UI/js/main.js
@@ -9,3 +9,26 @@ function closeSideNav(){
     document.getElementById("left-nav").style.width="0";
     document.getElementById("main").style.marginLeft="0";
 }
+
+// add a function to enable search of a product
+
+function searchInventory(){
+    // get the values being inserted in the searchbox and store them in a variable
+    var keywords=document.getElementById("search-box").value.toLowerCase();
+
+    // identify the element containers t be displayed if search text is found
+    var items=document.getElementsByClassName("crt-inv-item");
+
+    // iterate through the array of items searching for the keywords
+
+    for(i=0;i<items.length;i++){
+        if(items[i].innerText.toLowerCase().includes(keywords)){
+            // sho the ones that contain keywords.
+            items[i].style.display="block";
+        }
+        else{
+            // hide those that dont contain the keywords
+            items[i].style.display="none";
+        }
+    }
+}
