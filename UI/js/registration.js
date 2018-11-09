79390938ac7a939c9c7bc30fcaef04c5acbe7cd6
Meshack Ogeto <mesharkz1@gmail.com>
2018-11-09 16:45:48 +0300

    [#161838903] Add user email on top of every page

--- UI/js/registration.js
+++ UI/js/registration.js
@@ -1,6 +1,5 @@
-import {route} from 'routes.js';
+// import {route} from 'routes.js';
 
-document.getElementById('user-name').innerHTML = JSON.parse(sessionStorage.current_user).email;
 
 function addAttendant() {
     console.log(JSON.parse(sessionStorage.current_user))
